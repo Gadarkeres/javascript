13cccacf3d7560fd4461df1aebad8d39191224a3
Matheus Rafael <matheusrafael77@hotmail.com>
2023-06-06 15:06:36 -0300

    exercicio finalizado

---
+++ aula13 ex/ex014/script.js
@@ -0,0 +1,21 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('foto')
+    var data = new Date()
+    var hora = data.getHours()
+   
+
+
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+
+     if (hora >= 0 && hora < 12){
+        imagem.src = 'imagens/manha.png'
+        document.body.style.background ='#e2cd9f'
+    } else if (hora => 12 && hora <= 18){
+        imagem.src = 'imagens/tarde.png'
+        document.body.style.background = '#b9846f'
+    } else
+       imagem.src = 'imagens/noite.png'
+
+}
