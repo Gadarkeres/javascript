ae7a69526d6eb9dbef8bbda44107b4c96930c130
Matheus Rafael <matheusrafael77@hotmail.com>
2023-06-19 16:05:51 -0300

    Create ambiente.js

---
+++ aula18/ambiente.js
@@ -0,0 +1,7 @@
+var num = [5,8,9,3,1,3,2,1,3,2,3,5,6,7,8,9,6,5,4,3,2,1,4,35,6]
+/*num.sort()
+console.log(`nosso vetor é o ${num}`)
+console.log(`o primeiro valor do vetor é ${num[0]}`)
+// consolelog (num01)... num02...*/
+
+console.log(num.indexOf(3))
